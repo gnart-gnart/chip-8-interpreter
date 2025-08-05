aab590bd5f96ada3dd3ec0910ac78b691664c3c6
gnart-gnart <grantwoxford@gmail.com>
2025-08-05 17:56:36 -0500

    system font + initial implementations

---
+++ cpu.c
@@ -0,0 +1,46 @@
+#include "cpu.h"
+#include <string.h>
+
+void c8_init(Chip8Cpu *const cpu) {
+	c8_reset(cpu);
+	cpu->program_counter = START_ADDR;
+}
+
+void c8_reset(Chip8Cpu *const cpu) {
+	// reset V registers
+	cpu->v[0x0] = 0;
+	cpu->v[0x1] = 0;
+	cpu->v[0x2] = 0;
+	cpu->v[0x3] = 0;
+	cpu->v[0x4] = 0;
+	cpu->v[0x5] = 0;
+	cpu->v[0x6] = 0;
+	cpu->v[0x7] = 0;
+	cpu->v[0x8] = 0;
+	cpu->v[0x9] = 0;
+	cpu->v[0xA] = 0;
+	cpu->v[0xB] = 0;
+	cpu->v[0xC] = 0;
+	cpu->v[0xD] = 0;
+	cpu->v[0xE] = 0;
+	cpu->v[0xF] = 0;
+	// reset other registers
+	cpu->delay_timer = 0;
+	cpu->sound_timer = 0;
+	cpu->stack_pointer = 0;
+	cpu->index = 0;
+	cpu->program_counter = START_ADDR;
+	// other data
+	memset(cpu->stack, 0, sizeof(cpu->stack));
+	memset(cpu->keys, false, sizeof(cpu->keys));
+}
+
+void c8_push_stack(Chip8Cpu *const cpu, const word value) {
+	cpu->stack_pointer++;
+	cpu->stack[cpu->stack_pointer] = value;
+}
+
+void c8_pop_stack(Chip8Cpu *const cpu) {
+	cpu->stack[cpu->stack_pointer] = 0;
+	cpu->stack_pointer--;
+}
