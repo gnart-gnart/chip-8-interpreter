260092f70a0bfa2cc467027a2ed4bbb25e276932
Grant <grantwoxford@gmail.com>
2025-08-09 12:37:40 -0500

    changes

--- ui_ncurses.c
+++ ui_ncurses.c
@@ -1,37 +1,48 @@
 #include <ncurses.h>
 #include "ui.h"
 #include "global.h"
-// helper functions
+#include "string.h"
+
 bool prev_screen[SCREEN_WIDTH][SCREEN_HEIGHT];
+WINDOW *screen_win;
 
 // implementations
 void ui_init(const byte width, const byte height) {
-  initscr();
-  raw();
-  noecho();
-  nodelay(stdscr, 1);
-  curs_set(0);
-  
-  memset(prev_screen, 0, sizeof prev_screen);
+    initscr();
+    raw();
+    noecho();
+    nodelay(stdscr, 1);
+    curs_set(0);
+    screen_win = newwin(height + 2, width + 2, 0, 0);
+    box(screen_win, 0, 0);
+    wrefresh(screen_win);
+    memset(prev_screen, 0, sizeof prev_screen);
 }
 
 void ui_draw_screen(const bool screen[SCREEN_WIDTH][SCREEN_HEIGHT]) {
-    erase();
     for (byte y = 0; y < SCREEN_HEIGHT; y++) {
         for (int x = 0; x < SCREEN_WIDTH; x++) {
-            if (screen[x][y]) mvaddch(y, x, '#');
-            else mvaddch(y, x, ' ');
+            // update pixel only if it changed
+            if (prev_screen[x][y] != screen[x][y]) {
+                prev_screen[x][y] = screen[x][y];
+                if (screen[x][y]) mvwaddch(screen_win, y + 1, x + 1, '#');
+                else mvwaddch(screen_win, y + 1, x + 1, ' ');
+            }
         }
     }
-    refresh();
+    box(screen_win, 0, 0);
+    wrefresh(screen_win);
 }
 
 void ui_clear_screen(void) {
     erase();
-    refresh();
+    werase(screen_win);
+    box(screen_win, 0, 0);
+    wrefresh(screen_win);
 }
 
 void ui_deinit(void) {
+    delwin(screen_win);
     endwin();
 }
 
